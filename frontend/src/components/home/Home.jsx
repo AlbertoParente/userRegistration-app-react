e997f70562503cf04c08bd5679278726b4ed9f63
AlbertoParente <albertoparente20@gmail.com>
2021-09-08 20:42:34 -0300

    Main and home component

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props =>
+    <Main icon="home" title="Initial">
+        <div className="display-4">Welcome!</div>
+        <hr />
+        <p>System to exemplify the construction of a register in React!</p>
+
+    </Main>
