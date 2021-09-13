9f039fe8f17cbf8934a0b6295b39e509b8de4f0f
AlbertoParente <albertoparente20@gmail.com>
2021-09-12 21:17:20 -0300

    Create renderForm and updateField

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -31,16 +31,63 @@ export default class UserCrud extends Component {
                 this.setState({ user: initialState.user, list })
             })
     }
-
+    
     getUpdatedList(user) {
         const list = this.state.list.filter(u => u.id !== user.id)
         list.unshift(user)  
     }
 
+    updateField(event) {
+        const user = { ...this.staet.user }
+        uder[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nme</label>
+                            <input type="text" className="form-control" 
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={ e=> this.updateField(e) }
+                                placeholder="Enter the name...!" />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <input type="text" className="form-control" 
+                                    name="email"
+                                    value={this.state.user.email}
+                                    onChange={ e=> this.updateField(e) }
+                                    placeholder="Enter the e-mail...!" />
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={ e=> this.save(e) }>
+                            Save
+                        </button>
+                        <button className="btn btn-secondary ml-2"
+                            onClick={ e=> this.clear(e) }>
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <Main { ...headerProps }>
-                User Register
+                {this.renderForm()}
             </Main>
         )
     }
